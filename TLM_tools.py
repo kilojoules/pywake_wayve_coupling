a2d14b13b9b7b3e595d4fb95198a9da81e85f5b3
Dries Allaerts <dries.allaerts@kuleuven.be>
2018-05-17 16:03:41 +0200

    Add documentation TLM_tools.py

--- TLM_tools.py
+++ TLM_tools.py
@@ -13,8 +13,25 @@ import mpmath
 
 def Cg_cubic(hstar,z0_h,kappa):
     '''
-    Cg as a function of hstar=h*fc/utau and z0/h
-    corresponding to cubic viscosity profiles (Nieuwstadt
+    Geostrophic drag Cg = utau/G as a function of hstar = h*fc/utau and z0/h
+    corresponding to cubic viscosity profiles (Nieuwstadt 1983)
+
+    Parameters
+    ----------
+    hstar: float
+        Non-dimensional boundary-layer height
+        hstar = h*fc/utau
+    z0_h: float
+        Non-dimensional surface roughness length
+        z0_h = z0/h
+    kappa: float
+        Von Karman constant
+
+    Returns
+    -------
+    Cg: float
+        Geostrophic drag
+        Cg = utau/G
     '''
     F1 = F1_cubic(hstar,kappa)
     F2 = F2_cubic(hstar,kappa)
@@ -23,8 +40,24 @@ def Cg_cubic(hstar,z0_h,kappa):
 
 def alpha_cubic(hstar,z0_h,kappa):
     '''
-    alpha as a function of hstar=h*fc/utau and z0/h
-    corresponding to cubic viscosity profiles (Nieuwstadt
+    Geostrophic wind direction alpha as a function of hstar = h*fc/utau and z0/h
+    corresponding to cubic viscosity profiles (Nieuwstadt 1983)
+
+    Parameters
+    ----------
+    hstar: float
+        Non-dimensional boundary-layer height
+        hstar = h*fc/utau
+    z0_h: float
+        Non-dimensional surface roughness length
+        z0_h = z0/h
+    kappa: float
+        Von Karman constant
+
+    Returns
+    -------
+    alpha: float
+        Geostrophic wind direction
     '''
     Cg = Cg_cubic(hstar,z0_h,kappa)
     F2 = F2_cubic(hstar,kappa)
@@ -33,8 +66,20 @@ def alpha_cubic(hstar,z0_h,kappa):
 
 def F1_cubic(hstar,kappa):
     '''
-    F1 function corresponding to a cubic viscosity profile (Nieuwstadt)
-    hstar = h*fc/utau
+    F1 function corresponding to a cubic viscosity profile (Nieuwstadt 1983)
+
+    Parameters
+    ----------
+    hstar: float
+        Non-dimensional boundary-layer height
+        hstar = h*fc/utau
+    kappa: float
+        Von Karman constant
+
+    Returns
+    -------
+    F1: float
+        Value of F1 function
     '''
     C = hstar/kappa
     alpha = 0.5+0.5*np.sqrt(1+4j*C)
@@ -47,8 +92,20 @@ def F1_cubic(hstar,kappa):
 
 def F2_cubic(hstar,kappa):
     '''
-    F2 function corresponding to a cubic viscosity profile (Nieuwstadt)
-    hstar = h*fc/utau
+    F2 function corresponding to a cubic viscosity profile (Nieuwstadt 1983)
+
+    Parameters
+    ----------
+    hstar: float
+        Non-dimensional boundary-layer height
+        hstar = h*fc/utau
+    kappa: float
+        Von Karman constant
+
+    Returns
+    -------
+    F2: float
+        Value of F2 function
     '''
     C = hstar/kappa
     alpha = 0.5+0.5*np.sqrt(1+4j*C)
