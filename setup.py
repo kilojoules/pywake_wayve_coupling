1ad4f8af6ffa7aa55a02f54882c20b85c34a5a3c
u0138255 <koen.devesse@kuleuven.be>
2024-05-02 16:43:21 +0200

    Corrected setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='wayve',
     version='2.0.0',
-    packages=['wayve'],
+    packages=find_packages(exclude=['examples']),
     url='https://gitlab.kuleuven.be/TFSO-software/wayve',
     license='GNU AGPLv3',
     author='TFSO group',
