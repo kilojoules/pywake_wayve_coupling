b11150118fd76654f8cc695a8301024f0f3d5bfd
Julian Quick <juqu@dtu.dk>
2025-07-17 21:46:46 +0200

    refactor: Overhaul PyWakeInterface with caching and proper flow calculations
    - Add simulation caching to avoid redundant PyWake runs
    - Implement missing velocity field and xy_plane methods
    - Use proper PyWake classes for turbines and site definition
    - Fix wind direction handling and velocity conversions

--- wayve/couplings/pywake_coupling.py
+++ wayve/couplings/pywake_coupling.py
@@ -1,9 +1,13 @@
-
 import numpy as np
 from wayve.forcing.wind_farms.wake_model_coupling.wake_model_interface import UniDirectionalSelfSimilar
 import py_wake
 from py_wake.deficit_models.gaussian import BastankhahGaussianDeficit
+from py_wake.wind_turbines import WindTurbines
+from py_wake.site import UniformSite
+from foxes.utils import uv2wd
 from py_wake.deflection_models import JimenezWakeDeflection
+from py_wake.flow_map import HorizontalGrid
+from py_wake.flow_map import Points 
 from py_wake.wind_farm_models import PropagateDownwind, All2AllIterative
 from py_wake.turbulence_models import STF2005TurbulenceModel, STF2017TurbulenceModel, CrespoHernandez
 from py_wake.rotor_avg_models import RotorCenter, GridRotorAvg
@@ -13,201 +17,167 @@ from py_wake.wind_turbines import WindTurbine
 from py_wake.site.xrsite import XRSite
 import xarray as xr
 
-
 class PyWakeInterface(UniDirectionalSelfSimilar):
 
-    def __init__(self, k=0.04, deficit_model=BastankhahGaussianDeficit, rotor_avg_model=RotorCenter, 
-                 superposition_model=LinearSum, turbulence_model=CrespoHernandez, blockage_model=None,
-                 wind_farm_model=PropagateDownwind, deflection_model=JimenezWakeDeflection, deficit_kwargs=None):
+    def __init__(self, deficit_model=BastankhahGaussianDeficit, rotor_avg_model=RotorCenter(),
+                 superposition_model=LinearSum(), turbulence_model=CrespoHernandez(), deflection_model=JimenezWakeDeflection(),
+                 blockage_model=None, wind_farm_model=PropagateDownwind, deficit_kwargs={}):
 
-        # TODO: Needs more setup options (eg. wake model settings, turbulence model used, ...)
-        self.wake_deficit_model = deficit_model
+        self.deficit_model = deficit_model
         self.rotor_avg_model = rotor_avg_model
         self.superposition_model = superposition_model
         self.turbulence_model = turbulence_model
+        self.deflection_model = deflection_model
         self.blockage_model = blockage_model
         self.wind_farm_model = wind_farm_model
         self.deficit_kwargs = deficit_kwargs
-        self.deflection_model = deflection_model
-        self.blockage_model = blockage_model
-        self.__k = k
+        self.sim_res = None
 
-    @property
-    def k(self):
-        return self.__k
 
-    @k.setter
-    def k(self, value):
-        self.__k = value
-
-    def set_up_pywake_wind_farm(self, wind_farm, abl, u_bg_evaluator, apm_evaluator):
-        # Turbine hub locations
-        Nt = wind_farm.Nturb
-        xs = wind_farm.xs
-        ys = wind_farm.ys
-        hhs = [wind_farm.turbines[ii].zh for ii in range(Nt)]
-        locs = np.column_stack([xs, ys, hhs])
-        # Determine unperturbed background wind
-        hh = hhs[0]     # assume the same hub heights for now
-        U0 = abl.u(hh)
-        V0 = abl.v(hh)
-        # Wind speed
-        inflow_speed = np.sqrt(U0 ** 2 + V0 ** 2)
-        # Determine the main wind direction
-        e_str, e_span = self.background_flow_direction(wind_farm, abl)  # unit vector in main and spanwise wind directions
-        wd = np.arctan2(e_str[1], e_str[0])     # Wind direction in radians
-        # Determine background flow
-        background_flow = u_bg_evaluator(locs)
-        background_flow_str = background_flow[:, 0] * e_str[0] + background_flow[:, 1] * e_str[1]   # Project on wd
-        # PyWake call
-        dummy_speeds = np.linspace(1, 30)
-        site_ds = xr.Dataset(
+    def _run_simulation(self, wind_farm, abl, u_bg_evaluator):
+        """Helper function to run the simulation if it hasn't been run yet."""
+        if self.sim_res is not None:
+            return
+    
+        pywake_turbines = WindTurbines(
+            names=[f"Turbine_{i}" for i in range(wind_farm.Nturb)],
+            diameters=[t.D for t in wind_farm.turbines],
+            hub_heights=[t.zh for t in wind_farm.turbines],
+            powerCtFunctions=[
+                PowerCtTabular(
+                    ws=np.arange(0, 31),
+                    power=t.Cp(np.arange(0, 31)) * 0.5 * abl.rho * t.rotorarea * np.arange(0, 31)**3,
+                    power_unit='W',
+                    ct=t.Ct(np.arange(0, 31)))
+                for t in wind_farm.turbines
+            ]
+        )
+    
+        # Evaluate background flow at each turbine
+        turbine_coords = np.array([[t.x, t.y, t.zh] for t in wind_farm.turbines])
+        bg_vels = u_bg_evaluator(turbine_coords)
+        inflow_speeds_i = np.linalg.norm(bg_vels, axis=1)
+        inflow_wds_i = uv2wd(bg_vels)
+    
+        # For this uni-directional model, we perform a single simulation case.
+        # The representative wind direction and speed are the mean of the turbine inflows.
+        mean_wd = np.mean(inflow_wds_i)
+        mean_ws = np.mean(inflow_speeds_i)
+    
+        # Create a custom XRSite that holds the heterogeneous inflow data.
+        # This ensures PyWake uses the turbine-specific (i) values.
+        ds = xr.Dataset(
             data_vars={
-            'Speedup': ('i', background_flow_str / inflow_speed),
-            'P': 1,
-            'TI': abl.TI*100,
-            #'P': ('ws', np.ones_like(dummy_speeds) / dummy_speeds.size)
+                'WS': (('i',), inflow_speeds_i),
+                'WD': (('i',), inflow_wds_i),
+                'P': (('wd',), [1.0]),  # Probability
+                'TI': abl.TI            # Constant TI
             },
             coords={
-                'i': range(Nt),
-            'ws': dummy_speeds})
-        site = XRSite(site_ds, initial_position=np.array([xs, ys]).T)
+                'i': np.arange(wind_farm.Nturb),
+                'wd': [mean_wd]
+            }
+        )
+        site = XRSite(ds, default_ws=[mean_ws])
+    
+        wfm_kwargs = {
+            'site': site, 'windTurbines': pywake_turbines,
+            'wake_deficitModel': self.deficit_model(**self.deficit_kwargs),
+            'superpositionModel': self.superposition_model, 'deflectionModel': self.deflection_model,
+            'turbulenceModel': self.turbulence_model
+        }
+    
+        if self.blockage_model and self.wind_farm_model is not py_wake.wind_farm_models.PropagateDownwind:
+            wfm_kwargs['blockage_model'] = self.blockage_model
+    
+        wfm_instance = self.wind_farm_model(**wfm_kwargs)
+    
+        # The wd and ws arguments now select the single case defined in our custom site
+        self.sim_res = wfm_instance(
+            x=wind_farm.xs, y=wind_farm.ys, h=[t.zh for t in wind_farm.turbines],
+            type=np.arange(wind_farm.Nturb),
+            wd=[mean_wd],
+            ws=[mean_ws],
+            yaw=0, tilt=0
+        )
 
-        # assuming a single turbine type for now.
-        u = np.linspace(0, 30)
-        cp = wind_farm.turbines[0].Cp(u)
-        D = wind_farm.turbines[0].D
-        power = 1.225 * cp * D ** 2 * np.pi / 4
-        ct = wind_farm.turbines[0].Ct(u)
-        turbine = WindTurbine(name='wayve-turbine',
-                              diameter=D,
-                              hub_height=hh,
-                              powerCtFunction=PowerCtTabular(u, power, 'kW', ct))
 
-        if self.deficit_kwargs is None:
-           d_kwargs = {'k': self.k}
-        else:
-           d_kwargs = self.deficit_kwargs
-
-        if self.turbulence_model is None:
-           turb_model = None
-        else:
-           turb_model = self.turbulence_model()
-
-        # note that blockage only makes sense with All2All wind farm model
-        if self.blockage_model is None:
-           blockage = None
-        else:
-           blockage = self.blockage_model()
-
-        if self.blockage_model:
-
-            self.windFarmModel = self.wind_farm_model(
-                               site,
-                               turbine,
-                               wake_deficitModel=self.wake_deficit_model(**d_kwargs),
-                               superpositionModel=self.superposition_model(),
-                               deflectionModel=self.deflection_model(),
-                               turbulenceModel=turb_model,
-                               blockageModel=blockage)
-        else:
-            self.windFarmModel = self.wind_farm_model(
-                               site,
-                               turbine,
-                               wake_deficitModel=self.wake_deficit_model(**d_kwargs),
-                               superpositionModel=self.superposition_model(),
-                               deflectionModel=self.deflection_model(),
-                               turbulenceModel=turb_model)
-
-        sim_res = self.windFarmModel(xs, ys, ws=inflow_speed, wd=270. - np.rad2deg(wd), TI=abl.TI, yaw=0, tilt=0)
-        return (sim_res)
     def get_St_Ct_et(self, wind_farm, abl, u_bg_evaluator, apm_evaluator):
+        self._run_simulation(wind_farm, abl, u_bg_evaluator)
+        
+        effective_speeds = self.sim_res.WS_eff.to_numpy().flatten()
+        thrust_coefficients = self.sim_res.CT.to_numpy().flatten()
+        
+        e_str, _ = self.background_flow_direction(wind_farm, abl)
+        ets = np.tile(e_str, (wind_farm.Nturb, 1))
 
-        # set up wind farm
-        sim_res = self.set_up_pywake_wind_farm(wind_farm, abl, u_bg_evaluator, apm_evaluator)
-
-        effective_speeds = sim_res.WS_eff.to_numpy()[:, 0, 0]
-        Nt = sim_res.x.size
-        thrust_coefficients = np.zeros(Nt)
-        ets = np.zeros((Nt, 2))
-        for i in range(Nt):
-            thrust_coefficients[i] = wind_farm.turbines[i].Ct(effective_speeds[i])
-            ets[i] = np.array([np.cos(sim_res.wd), np.sin(sim_res.wd)]).flatten()
         return effective_speeds, thrust_coefficients, ets
-
-    def get_u_subgrid(self, wind_farm, abl, u_bg_evaluator, apm_evaluator, subgrid):
-        """
-        Calculate the wake model velocity field on the given subgrid.
-
-        This method will only be called after the method get_St_Ct_et has been called, and will use the same inputs.
-        Therefore, feel free to store any intermediate results that can be re-used in this calculation.
-
-        The basic turbine information can be found in the WindFarm object. The unperturbed atmospheric state can be
-        found in the ABL object. Additionally, the u_bg_evaluator is a callable that can evaluate the background
-        velocities at requested locations. Finally, the apm_evaluator is a callable that can evaluate the lower-layer
-        APM wind speeds and height at requested locations.
-
-        Parameters
-        ----------
-        wind_farm    WindFarm object
-            Wind farm for which the calculation is performed
-        abl     ABL object
-            Information on the unperturbed background state, and atmospheric variables such as TI and z0.
-        u_bg_evaluator  callable
-            Callable which can evaluate the background velocities at requested locations.
-            See VaryingBackground.set_up_u_bg_evaluator in varying_background.py for detailed documentation.
-        apm_evaluator   callable
-            Callable which can evaluate the layer wind speeds and height at requested locations.
-            See VaryingBackground.set_up_apm_evaluators in varying_background.py for detailed documentation.
-        subgrid     SubGrid object
-            Grid on which the velocity should be evaluated
-
-        Returns
-        -------
-        u_wm  np.array
-            X-components of the wake model velocity field, defined on the subgrid (shape same as subgrid.shape)
-        v_wm  np.array
-            Y-components of the wake model velocity field, defined on the subgrid (shape same as subgrid.shape)
-        """
-        sim_res = self.set_up_pywake_wind_farm(self, wind_farm, abl, u_bg_evaluator, apm_evaluator)
-        # TODO: Implement this method
-        raise NotImplementedError("Velocity calculation not implemented yet!")
 
     def wake_deficit(self, wind_farm, abl, u_bg_evaluator, apm_evaluator, subgrid):
         """
         Calculate the wake deficit, calculated on the given subgrid.
-
+    
         The wake deficit is the factor that, when multiplied with the background velocity, gives the wake model
-        velocity. For the uni-directional wake merging method of Lanzilao and Meyers (Wind Energy, 2022), this
-        corresponds to the product over all wake functions (1-W_k).
+        velocity.
         """
-        # Get main streamwise direction #
-        e_str, e_span = self.background_flow_direction(wind_farm, abl)
-        # Get wake model velocities #
-        # XY components
-        u_wm, v_wm = self.get_u_subgrid(wind_farm, abl, u_bg_evaluator, apm_evaluator, subgrid)
-        # Map onto streamwise direction
-        str_wm = u_wm * e_str[0] + v_wm * e_str[1]
-        # Get background velocities #
-        # SubGrid coordinates
-        locations = subgrid.locations
-        # Evaluate background velocities
-        vel_bg = u_bg_evaluator(locations)
-        # Reshape into grid
-        Nx, Ny, Nz = subgrid.shape
-        u_bg = np.reshape(vel_bg[:, 0], (Nx, Ny, Nz))
-        v_bg = np.reshape(vel_bg[:, 1], (Nx, Ny, Nz))
-        # Map onto streamwise direction
-        str_bg = u_bg * e_str[0] + v_bg * e_str[1]
-        # Calculate wake deficit #
-        w = np.divide(str_wm, str_bg)
-        return w
+        # Ensure the simulation has been run
+        self._run_simulation(wind_farm, abl, u_bg_evaluator)
+    
+        # Convert WAYVE's subgrid to a PyWake-compatible grid
+        locs = subgrid.locations
+        pywake_grid = Points(x=locs[:, 0], y=locs[:, 1], h=locs[:, 2])
+    
+        # Get the flow map for all points in the subgrid
+        flow_map = self.sim_res.flow_map(grid=pywake_grid)
+    
+        # The wake deficit is the ratio of effective wind speed to ambient wind speed
+        wake_deficit = flow_map.WS_eff.values / flow_map.WS.values
+    
+        # Reshape the result to match the original subgrid shape
+        wake_deficit = wake_deficit.reshape(subgrid.shape)
+    
+        return wake_deficit
+
+
+    def get_u_subgrid(self, wind_farm, abl, u_bg_evaluator, apm_evaluator, subgrid):
+        """
+        Calculates the full velocity field on the subgrid. This is now called
+        directly by the generic WakeModelVelocityHandler.
+        """
+        self._run_simulation(wind_farm, abl, u_bg_evaluator)
+
+        # Get the flow map for all points in the subgrid
+        flow_map = self.sim_res.flow_map(grid=subgrid)
+        
+        # Get wind speed and direction, then convert to u, v components
+        ws_grid = flow_map.WS_eff.values
+        wd_grid = flow_map.WD.values
+        wd_rad = np.deg2rad(270.0 - wd_grid)
+        
+        u_wm = ws_grid * np.cos(wd_rad)
+        v_wm = ws_grid * np.sin(wd_rad)
+
+        return u_wm, v_wm
 
     def xy_plane(self, wind_farm, abl, u_bg_evaluator, apm_evaluator, xs, ys, z):
-        """
-        Compute an xy cross-section of the flow.
-        """
-        # TODO: Implement this method
-        raise NotImplementedError("Velocity calculation not implemented yet!")
+        """This method also reuses the stored simulation for 2D visualization slices."""
+        self._run_simulation(wind_farm, abl, u_bg_evaluator)
 
+        grid = HorizontalGrid(x=xs, y=ys, h=z)
+        flow_map = self.sim_res.flow_map(grid=grid)
 
+        # Ambient (background) velocities
+        amb_ws = flow_map.WS.values.squeeze()
+        amb_wd = flow_map.WD.values.squeeze()
+        amb_wd_rad = np.deg2rad(270.0 - amb_wd)
+        amb_u = (amb_ws * np.cos(amb_wd_rad))
+        amb_v = (amb_ws * np.sin(amb_wd_rad))
+
+        # Waked velocities
+        ws = flow_map.WS_eff.values.squeeze()
+        wd = flow_map.WD.values.squeeze()
+        wd_rad = np.deg2rad(270.0 - wd)
+        u_wm = (ws * np.cos(wd_rad))
+        v_wm = (ws * np.sin(wd_rad))
+
+        return amb_u, amb_v, u_wm, v_wm
